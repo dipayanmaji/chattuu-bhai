5f053d445a32aa403e149170b30f28ec0bb6ca01
dipayanmaji <dipayanmaji1112@gmail.com>
2024-03-13 03:16:28 +0530

    message box design done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import ChatBox from './components/ChatBox';
 import ParticleBg from './components/ParticleBg';
@@ -17,17 +17,18 @@ function App() {
     <div className={darkMode ? 'dark' : ''}>
       <div className='w-full h-screen md:px-10 px-4 py-[2%] bg-gradient-to-r from-violet-500 to-fuchsia-500 dark:from-black dark:to-black text-zinc-100 dark:text-zinc-200 relative'>
 
-        <header className='h-[10%] flex items-center justify-between px-2 py-4 mb-4 dark:bg-neutral-900/50 bg-gray-500/50 w-full text-2xl rounded-lg relative z-10'>
-          <h1>Chattuu Bhai</h1>
-          <span onClick={darkModeHandler} className='cursor-pointer'>{
+        <header className='w-full max-w-4xl h-[10%] mx-auto flex items-center justify-between p-4 mb-4 dark:bg-neutral-900/50 bg-gray-500/50 text-2xl rounded-lg relative z-10'>
+          <h1 className='dark:text-sky-300 text-white'>Chattuu Bhai</h1>
+          <span onClick={darkModeHandler} className='cursor-pointer dark:text-sky-300 text-white'>{
             darkMode ? <IoIosSunny /> : <IoMdMoon />
           }</span>
         </header>
 
         <ChatBox />
 
-        <Globe />
-        <ParticleBg />
+        {/* <Globe />
+        <ParticleBg /> */}
+
       </div>
     </div>
   );
--- src/components/ChatBox/index.jsx
+++ src/components/ChatBox/index.jsx
@@ -1,8 +1,143 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+
+// const messages = [
+//     {
+//         text: "hii this is chattuu bhai",
+//         time: "01:03 PM"
+//     },
+//     {
+//         text: "hii Whatsupp",
+//         time: "11:10 PM"
+//     },
+//     {
+//         text: "Hello Brother",
+//         time: "11:03 PM"
+//     },
+//     {
+//         text: "hii this is chattuu bhai",
+//         time: "01:03 PM"
+//     },
+//     {
+//         text: "hii how are you",
+//         time: "10:33 PM"
+//     },
+//     {
+//         text: "hii Whatsupp",
+//         time: "11:10 PM"
+//     },
+//     {
+//         text: "heyy chattuu bhai here",
+//         time: "05:03 PM"
+//     },
+//     {
+//         text: "when you go to bed chattuu bhai!",
+//         time: "03:30 PM"
+//     },
+//     {
+//         text: "hii this is chattuu bhai",
+//         time: "01:03 PM"
+//     },
+// ]
 
 const ChatBox = () => {
+    const [messages, setMessages] = useState([
+        {
+            text: "hii this is chattuu bhai",
+            time: "01:03 PM"
+        },
+        {
+            text: "hii Whatsupp",
+            time: "11:10 PM"
+        },
+        {
+            text: "Hello Brother",
+            time: "11:03 PM"
+        },
+        {
+            text: "hii this is chattuu bhai",
+            time: "01:03 PM"
+        },
+        {
+            text: "hii how are you",
+            time: "10:33 PM"
+        },
+        {
+            text: "hii Whatsupp",
+            time: "11:10 PM"
+        },
+        {
+            text: "heyy chattuu bhai here",
+            time: "05:03 PM"
+        },
+        {
+            text: "when you go to bed chattuu bhai!",
+            time: "03:30 PM"
+        },
+        {
+            text: "hii this is chattuu bhai",
+            time: "01:03 PM"
+        },
+    ]);
+    const [newMsg, setNewMsg] = useState('');
+    const inputRef = useRef(null);
+
+    const createMessage = () => {
+        inputRef.current.focus();
+        if (newMsg === '') return;
+
+        const date = new Date();
+        let xm = "AM";
+        let hh = date.getHours();
+        if (hh > 12) {
+            hh = hh - 12;
+            xm = "PM";
+        }
+        if (hh < 10) hh = "0" + hh;
+        let mm = date.getMinutes();
+        if (mm < 10) mm = "0" + mm;
+
+        let time = `${hh}:${mm} ${xm}`
+
+        setMessages([...messages, {
+            text: newMsg,
+            time: time
+        }]);
+
+        setNewMsg('');
+    }
+
     return (
-        <div className="w-full h-[87%] dark:bg-neutral-900/50 bg-gray-500/30 rounded-lg relative z-10">
+        <div className="w-full max-w-4xl h-[87%] mx-auto dark:bg-neutral-900/50 bg-gray-500/30 rounded-lg relative z-10 overflow-hidden">
+
+            <div className="w-full h-[calc(100%-3.5rem)] px-4 pt-2 mb-2 space-y-2 overflow-auto">
+                {
+                    messages.map((msg, index) =>
+                        <div className="w-full min-h-12 dark:bg-gray-500/10 bg-white/30 dark:text-white text-neutral-800 rounded-xl px-4 pt-2 pb-6">
+                            <p>{msg.text}</p>
+                            <span className="text-sm float-end inline-block">{msg.time}</span>
+                        </div>
+                    )
+                }
+            </div>
+
+            <div className="w-full h-12 min-h-12 px-4 pb-2">
+                <input
+                    ref={inputRef}
+                    onChange={(e) => setNewMsg(e.currentTarget.value)}
+                    value={newMsg}
+                    autoFocus
+                    type="text"
+                    placeholder="Message"
+                    className="w-[calc(100%-5.5rem)] mr-2 h-full dark:bg-gray-500/10 bg-white/30 dark:text-white text-neutral-800 dark:placeholder-slate-300/80 placeholder-slate-800/80 rounded-full px-4 py-1 outline-none"
+                />
+                <button
+                    onClick={createMessage}
+                    className="w-20 h-full dark:bg-gray-500/10 bg-white/30 dark:text-white text-neutral-800 rounded-full px-4 py-1"
+                >
+                    Send
+                </button>
+            </div>
+
         </div>
     )
 }
